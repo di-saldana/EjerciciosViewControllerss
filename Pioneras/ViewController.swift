161a828293e7bc2d8a43a21c36ff4e2d87743e1f
di-saldana <dsl42@alu.ua.es>
2023-11-12 23:15:44 +0100

    Pantalla secundaria y navegacion

--- Pioneras/ViewController.swift
+++ Pioneras/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func retornoDeSecundaria(segue: UIStoryboardSegue) {
+        print("Volviendo atrás por \(segue.identifier)...")
+    }
+    
 
 }
 
